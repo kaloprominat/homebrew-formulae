6786f587c2881c1cda382c5fb684d45cf994c7b0
Pavel Kondratiev <kaloprominat@gmail.com>
2014-08-22 16:39:34 +0400

    Update brew-pkg.rb
    updating due to fork

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class BrewPkg < Formula
-  homepage 'https://github.com/timsutton/brew-pkg'
-  url 'https://github.com/timsutton/brew-pkg.git', :tag => 'v0.1.5'
-  version '0.1.5'
+  homepage 'https://github.com/kaloprominat/brew-pkg'
+  url 'https://github.com/kaloprominat/brew-pkg.git', :tag => 'v0.1.5'
+  version '0.1.6'
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
