9c8850e1d0d93a139d8052477a82bac5b30e3941
Pavel Kondratiev <kaloprominat@gmail.com>
2014-08-23 02:19:38 +0400

    Update brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class BrewPkg < Formula
   homepage 'https://github.com/kaloprominat/brew-pkg'
-  url 'https://github.com/kaloprominat/brew-pkg.git', :tag => 'v0.1.8'
-  version '0.1.8'
+  url 'https://github.com/kaloprominat/brew-pkg.git', :tag => 'v0.1.9'
+  version '0.1.9'
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
