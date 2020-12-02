cf5078577d340531e1f40175c9a93ab45b1a530d
Pavel Kondratiev <kaloprominat@yandex.ru>
2020-12-02 16:10:09 +0300

    Update brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class BrewPkg < Formula
   homepage 'https://github.com/kaloprominat/brew-pkg'
-  url 'https://github.com/kaloprominat/brew-pkg.git', :tag => 'v0.1.10'
+  url 'https://github.com/kaloprominat/brew-pkg.git', :tag => 'master'
   version '0.1.10'
 
   # This is an .rb that must be executable in order for Homebrew to
